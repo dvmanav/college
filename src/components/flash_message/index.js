31248d27520d42fe5f5077252adfb24b6f592a71
Manav Sethi <dev.manav.sethi@gmail.com>
2019-10-19 17:54:40 +0530

    added hide button for flash message

--- src/components/flash_message/index.js
+++ src/components/flash_message/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 class FlashMessage extends React.Component {
     constructor(props){
         super(props);
-        this.state = {message:this.props.message, color: this.props.color};
+        this.state = {message:this.props.message, color: this.props.color, hidden:false};
     }
     
     render(){
-       return (
-        <div>
-           <p style={{backgroundColor:this.state.color}}>{this.state.message}</p>
-        </div>
-        );
+        const {message, color, hidden} = this.state;
+        if(hidden){
+            return (
+                <div></div>
+            );
+
+        } else if(!hidden){
+            return (
+                <div>
+                   <p style={{backgroundColor:color}}>{message}</p>
+                   <button onClick={() => {this.setState({hidden:true})}}>Hide</button>
+                </div>
+                );
+        }
+       
     }
 }
 export default FlashMessage;
