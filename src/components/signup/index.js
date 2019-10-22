8a78c5a398e897b63acdd21773320f06a5cdc013
Manav Sethi <dev.manav.sethi@gmail.com>
2019-10-22 11:22:42 +0530

    added signup form

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -15,12 +15,16 @@ class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      gender: '',
+      user: {
+        name: null,
+        email: null,
+        password: null,
+        date_of_birth: null,
+        gender: null
+      },
       SignedUp: '',
       error: ''
     };
-
-    this.Signup = this.Signup.bind(this);
   }
 
   render() {
@@ -31,23 +35,23 @@ class Signup extends React.Component {
     return (
       <div>
       {
-        SignedUp.localeCompare("failed")?
+        SignedUp.localeCompare("failed") ?
         <FlashMessage message = {error} color = "red"></FlashMessage> : <div></div>
       }
         <h2> User Signup </h2>
-        <form onSubmit={ this.Signup }>
-          <input type="text" ref="name" placeholder="Name" required></input>
+        <form onSubmit={(e) => {this.Signup(e)} }>
+          <input type="text" name="name" placeholder="Name" onChange={(e) => {this.UpdateUser(e)}} required></input>
           <br /><br />
-          <input type="email" ref="email" placeholder="Email" required></input>
+          <input type="email" name="email" placeholder="Email" onChange={(e) => {this.UpdateUser(e)}} required></input>
           <br /><br />
-          <input type="password" ref="password" placeholder="Password" required></input>
+          <input type="password" name="password" placeholder="Password" onChange={(e) => {this.UpdateUser(e)}} required></input>
           <br /><br />
           <label for="date_of_birth">Date of Birth: </label>
-          <input type="date" ref="date_of_birth" required></input>
+          <input type="date" name="date_of_birth" onChange={(e) => {this.UpdateUser(e)}} required></input>
           <br /><br />
           <label for="gender">Gender: </label>
-          <input type="radio" value="MALE" defaultChecked onChange={ this.getGender.bind(this) } name="gender" /> Male
-          <input type="radio" value="FEMALE" onChange={ this.getGender.bind(this) } name="gender" /> Female
+          <input type="radio" name="gender" value="MALE" defaultChecked onChange={(e) => {this.UpdateUser(e)}} name="gender" /> Male
+          <input type="radio" name="gender" value="FEMALE" onChange={(e) => {this.UpdateUser(e)}} name="gender" /> Female
           <br /><br />
           <input type="submit" value="Signup"></input>
           <br /><br />
@@ -58,22 +62,29 @@ class Signup extends React.Component {
     );
   }
 
-  getGender(event) {
+  UpdateUser(event) {
+    var key=  event.target.name;
+    var value =event.target.value;
+    console.log(key  + " : "  +value);
     this.setState({
-      gender: event.target.value
+      user: {
+        key : value
+      }
     })
   }
 
   Signup(event) {
     event.preventDefault();
+    const {user, SignedUp, error} = this.state;
+
     fetch(process.env.REACT_APP_NAVEEN_API_URL + '/v1/users', {
         method: 'POST',
         body: JSON.stringify({
-          "user[email]": this.refs.email.value,
-          "user[password]": this.refs.password.value,
-          "user[name]": this.refs.name.value,
-          "user[date_of_birth]": this.refs.date_of_birth.value,
-          "user[gender]": this.state.gender
+          "user[email]": user.email,
+          "user[password]": user.password,
+          "user[name]": user.name,
+          "user[date_of_birth]": user.date_of_birth,
+          "user[gender]": user.gender
         })
       }).then(res => res.json())
       .then(
